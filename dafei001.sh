4617ec4c4719f494ab046e3cd3077baa2f70bc49
wangyifei <243005761@qq.com>
2019-03-06 18:18:16 +0800

    Update dafei001.sh
    第一次更改

--- dafei001.sh
+++ dafei001.sh
@@ -1,4 +1,17 @@
-ls
-ls
-df
-df
+#!/usr/bin/env python
+#_*_ coding:utf-8 _*_
+
+import random
+
+real_age = (random.randint(1,20))
+
+for i in range(5):
+    age = input('age:')
+    if age > real_age :
+      print 'think smaller!'
+    elif age == real_age :
+      print '\033[31;1mGood! you are right!\033[0m'
+      break
+    else :
+      print 'think bigger!'
+    print 'you still got %d shots!' %(4 - i)
